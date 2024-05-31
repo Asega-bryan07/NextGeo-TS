e47f6525bf053908772a2234f2f456da5b190258
Asega-bryan07 <Almasibryan7@gmail.com>
2024-06-01 01:48:27 +0300

    complete: redirect, profile and add-servers

--- nextgeo/components/navigation/navigation-sidebar.tsx
+++ nextgeo/components/navigation/navigation-sidebar.tsx
@@ -1,6 +1,12 @@
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./navigation-item"
+
+
 import { redirect } from "next/navigation";
+import { NavigationAction } from "./navigation-action";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -23,7 +29,22 @@ export const NavigationSidebar = async () => {
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full
         dark:bg-[#1E1F22]">
-            Navigation Sidebar
+            <NavigationAction/>
+            <Separator
+            className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem
+                        id={server.id}
+                        name={server.name}
+                        imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+
+            </ScrollArea>
         </div>
     )
 }
